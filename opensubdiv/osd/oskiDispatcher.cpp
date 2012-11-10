e287ed5dd561e1697d7a928f715bc1c3aacddb97
Michael Driscoll <mbdriscoll@gmail.com>
2012-11-10 00:31:03 -0800

    make OskiKernelDispatcher a subclass of CpuKernelDispatcher

--- opensubdiv/osd/oskiDispatcher.cpp
+++ opensubdiv/osd/oskiDispatcher.cpp
@@ -29,25 +29,8 @@ using namespace boost::numeric::ublas;
 namespace OpenSubdiv {
 namespace OPENSUBDIV_VERSION {
 
-OsdOskiKernelDispatcher::Table::~Table() {
-
-    if (ptr)
-        free(ptr);
-}
-
-void
-OsdOskiKernelDispatcher::Table::Copy( int size, const void *table ) {
-
-    if (size > 0) {
-        if (ptr)
-            free(ptr);
-        ptr = malloc(size);
-        memcpy(ptr, table, size);
-    }
-}
-
 OsdOskiKernelDispatcher::OsdOskiKernelDispatcher( int levels )
-    : OsdKernelDispatcher(levels), A_tunable(NULL), _currentVertexBuffer(NULL), _currentVaryingBuffer(NULL), _vdesc(NULL) {
+    : OsdCpuKernelDispatcher(levels), A_tunable(NULL) {
     _tables.resize(TABLE_MAX);
     M = NULL;
     S = NULL;
@@ -60,63 +43,6 @@ OsdOskiKernelDispatcher::~OsdOskiKernelDispatcher() {
 
     if (_vdesc)
         delete _vdesc;
-}
-
-static OsdOskiKernelDispatcher::OsdKernelDispatcher *
-Create(int levels) {
-    return new OsdOskiKernelDispatcher(levels);
-}
-
-void
-OsdOskiKernelDispatcher::Register() {
-    Factory::GetInstance().Register(Create, kOSKI);
-}
-
-void
-OsdOskiKernelDispatcher::OnKernelLaunch() {
-}
-
-void
-OsdOskiKernelDispatcher::OnKernelFinish() {
-}
-
-void
-OsdOskiKernelDispatcher::CopyTable(int tableIndex, size_t size, const void *ptr) {
-
-    _tables[tableIndex].Copy((int)size, ptr);
-}
-
-void
-OsdOskiKernelDispatcher::AllocateEditTables(int n) {
-
-    _editTables.resize(n*2);
-    _edits.resize(n);
-}
-
-void
-OsdOskiKernelDispatcher::UpdateEditTable(int tableIndex, const FarTable<unsigned int> &offsets, const FarTable<float> &values,
-                                        int operation, int primVarOffset, int primVarWidth) {
-
-    _editTables[tableIndex*2+0].Copy(offsets.GetMemoryUsed(), offsets[0]);
-    _editTables[tableIndex*2+1].Copy(values.GetMemoryUsed(), values[0]);
-
-    _edits[tableIndex].offsetOffsets.resize(_maxLevel);
-    _edits[tableIndex].valueOffsets.resize(_maxLevel);
-    _edits[tableIndex].numEdits.resize(_maxLevel);
-    for (int i = 0; i < _maxLevel; ++i) {
-        _edits[tableIndex].offsetOffsets[i] = (int)(offsets[i] - offsets[0]);
-        _edits[tableIndex].valueOffsets[i] = (int)(values[i] - values[0]);
-        _edits[tableIndex].numEdits[i] = offsets.GetNumElements(i);
-    }
-    _edits[tableIndex].operation = operation;
-    _edits[tableIndex].primVarOffset = primVarOffset;
-    _edits[tableIndex].primVarWidth = primVarWidth;
-}
-
-OsdVertexBuffer *
-OsdOskiKernelDispatcher::InitializeVertexBuffer(int numElements, int numVertices)
-{
-    return new OsdCpuVertexBuffer(numElements, numVertices);
 }
 
 void
@@ -133,8 +59,18 @@ OsdOskiKernelDispatcher::BindVertexBuffer(OsdVertexBuffer *vertex, OsdVertexBuff
         _currentVaryingBuffer = NULL;
 
     _vdesc = new OskiVertexDescriptor(this,
-            _currentVertexBuffer ? _currentVertexBuffer->GetNumElements() : 0,
+            _currentVertexBuffer  ? _currentVertexBuffer->GetNumElements()  : 0,
             _currentVaryingBuffer ? _currentVaryingBuffer->GetNumElements() : 0);
+}
+
+static OsdOskiKernelDispatcher::OsdKernelDispatcher *
+Create(int levels) {
+    return new OsdOskiKernelDispatcher(levels);
+}
+
+void
+OsdOskiKernelDispatcher::Register() {
+    Factory::GetInstance().Register(Create, kOSKI);
 }
 
 void
@@ -184,6 +120,7 @@ OsdOskiKernelDispatcher::ApplyM(int offset)
 {
     if (A_tunable == NULL) {
         assert(M != NULL);
+        assert(_currentVertexBuffer != NULL);
 
         int numElems = _currentVertexBuffer->GetNumElements();
         float* V_in = _currentVertexBuffer->GetCpuBuffer();
@@ -243,130 +180,6 @@ OsdOskiKernelDispatcher::WriteM()
                 fprintf(ofile, "%d %d %10.3g\n", i/6+1, j/6+1, (float) (*M)(i,j));
 
     fclose(ofile);
-}
-
-void
-OsdOskiKernelDispatcher::UnbindVertexBuffer()
-{
-    delete _vdesc;
-    _vdesc = NULL;
-
-    _currentVertexBuffer = NULL;
-    _currentVaryingBuffer = NULL;
-}
-
-void
-OsdOskiKernelDispatcher::Synchronize() { }
-
-
-void
-OsdOskiKernelDispatcher::ApplyBilinearFaceVerticesKernel( FarMesh<OsdVertex> * mesh, int offset, int level, int start, int end, void * data) const {
-
-    computeFace(_vdesc, GetVertexBuffer(), GetVaryingBuffer(),
-                (int*)_tables[F_IT].ptr + _tableOffsets[F_IT][level-1],
-                (int*)_tables[F_ITa].ptr + _tableOffsets[F_ITa][level-1],
-                offset, start, end);
-}
-
-void
-OsdOskiKernelDispatcher::ApplyBilinearEdgeVerticesKernel( FarMesh<OsdVertex> * mesh, int offset, int level, int start, int end, void * data) const {
-
-    computeBilinearEdge(_vdesc, GetVertexBuffer(), GetVaryingBuffer(),
-                        (int*)_tables[E_IT].ptr + _tableOffsets[E_IT][level-1],
-                        offset,
-                        start, end);
-}
-
-void
-OsdOskiKernelDispatcher::ApplyBilinearVertexVerticesKernel( FarMesh<OsdVertex> * mesh, int offset, int level, int start, int end, void * data) const {
-
-    computeBilinearVertex(_vdesc, GetVertexBuffer(), GetVaryingBuffer(),
-                          (int*)_tables[V_ITa].ptr + _tableOffsets[V_ITa][level-1],
-                          offset, start, end);
-}
-
-void
-OsdOskiKernelDispatcher::ApplyCatmarkFaceVerticesKernel( FarMesh<OsdVertex> * mesh, int offset, int level, int start, int end, void * data) const {
-
-    computeFace(_vdesc, GetVertexBuffer(), GetVaryingBuffer(),
-                (int*)_tables[F_IT].ptr + _tableOffsets[F_IT][level-1],
-                (int*)_tables[F_ITa].ptr + _tableOffsets[F_ITa][level-1],
-                offset, start, end);
-}
-
-void
-OsdOskiKernelDispatcher::ApplyCatmarkEdgeVerticesKernel( FarMesh<OsdVertex> * mesh, int offset, int level, int start, int end, void * data) const {
-
-    computeEdge(_vdesc, GetVertexBuffer(), GetVaryingBuffer(),
-                (int*)_tables[E_IT].ptr + _tableOffsets[E_IT][level-1],
-                (float*)_tables[E_W].ptr + _tableOffsets[E_W][level-1],
-                offset,
-                start, end);
-}
-
-void
-OsdOskiKernelDispatcher::ApplyCatmarkVertexVerticesKernelB( FarMesh<OsdVertex> * mesh, int offset, int level, int start, int end, void * data) const {
-
-    computeVertexB(_vdesc, GetVertexBuffer(), GetVaryingBuffer(),
-                   (int*)_tables[V_ITa].ptr + _tableOffsets[V_ITa][level-1],
-                   (int*)_tables[V_IT].ptr + _tableOffsets[V_IT][level-1],
-                   (float*)_tables[V_W].ptr + _tableOffsets[V_W][level-1],
-                   offset, start, end);
-}
-
-void
-OsdOskiKernelDispatcher::ApplyCatmarkVertexVerticesKernelA( FarMesh<OsdVertex> * mesh, int offset, bool pass, int level, int start, int end, void * data) const {
-
-    computeVertexA(_vdesc, GetVertexBuffer(), GetVaryingBuffer(),
-                   (int*)_tables[V_ITa].ptr + _tableOffsets[V_ITa][level-1],
-                   (float*)_tables[V_W].ptr + _tableOffsets[V_W][level-1],
-                   offset, start, end, pass);
-}
-
-void
-OsdOskiKernelDispatcher::ApplyLoopEdgeVerticesKernel( FarMesh<OsdVertex> * mesh, int offset, int level, int start, int end, void * data) const {
-
-    computeEdge(_vdesc, GetVertexBuffer(), GetVaryingBuffer(),
-                (int*)_tables[E_IT].ptr + _tableOffsets[E_IT][level-1],
-                (float*)_tables[E_W].ptr + _tableOffsets[E_W][level-1],
-                offset,
-                start, end);
-}
-
-void
-OsdOskiKernelDispatcher::ApplyLoopVertexVerticesKernelB( FarMesh<OsdVertex> * mesh, int offset, int level, int start, int end, void * data) const {
-
-    computeLoopVertexB(_vdesc, GetVertexBuffer(), GetVaryingBuffer(),
-                       (int*)_tables[V_ITa].ptr + _tableOffsets[V_ITa][level-1],
-                       (int*)_tables[V_IT].ptr + _tableOffsets[V_IT][level-1],
-                       (float*)_tables[V_W].ptr + _tableOffsets[V_W][level-1],
-                       offset, start, end);
-}
-
-void
-OsdOskiKernelDispatcher::ApplyLoopVertexVerticesKernelA( FarMesh<OsdVertex> * mesh, int offset, bool pass, int level, int start, int end, void * data) const {
-
-    computeVertexA(_vdesc, GetVertexBuffer(), GetVaryingBuffer(),
-                   (int*)_tables[V_ITa].ptr + _tableOffsets[V_ITa][level-1],
-                   (float*)_tables[V_W].ptr + _tableOffsets[V_W][level-1],
-                   offset, start, end, pass);
-}
-
-void
-OsdOskiKernelDispatcher::ApplyVertexEdits(FarMesh<OsdVertex> *mesh, int offset, int level, void * clientdata) const {
-    for (int i=0; i<(int)_edits.size(); ++i) {
-        const VertexEditArrayInfo &info = _edits[i];
-
-        if (info.operation == FarVertexEdit::Add) {
-            editVertexAdd(_vdesc, GetVertexBuffer(), info.primVarOffset, info.primVarWidth, info.numEdits[level-1],
-                          (int*)_editTables[i*2+0].ptr + info.offsetOffsets[level-1],
-                          (float*)_editTables[i*2+1].ptr + info.valueOffsets[level-1]);
-        } else if (info.operation == FarVertexEdit::Set) {
-            editVertexSet(_vdesc, GetVertexBuffer(), info.primVarOffset, info.primVarWidth, info.numEdits[level],
-                          (int*)_editTables[i*2+0].ptr + info.offsetOffsets[level],
-                          (float*)_editTables[i*2+1].ptr + info.valueOffsets[level]);
-        }
-    }
 }
 
 } // end namespace OPENSUBDIV_VERSION
--- opensubdiv/osd/oskiDispatcher.h
+++ opensubdiv/osd/oskiDispatcher.h
@@ -2,7 +2,7 @@
 #define OSD_OSKI_DISPATCHER_H
 
 #include "../version.h"
-#include "../osd/kernelDispatcher.h"
+#include "../osd/cpuDispatcher.h"
 
 extern "C" {
     #include <oski/oski_Tis.h>
@@ -21,100 +21,28 @@ namespace OPENSUBDIV_VERSION {
 
 struct OskiVertexDescriptor;
 
-class OsdOskiKernelDispatcher : public OsdKernelDispatcher
+class OsdOskiKernelDispatcher : public OsdCpuKernelDispatcher
 {
 public:
     OsdOskiKernelDispatcher(int levels);
-
     virtual ~OsdOskiKernelDispatcher();
-
-
-    virtual void ApplyBilinearFaceVerticesKernel(FarMesh<OsdVertex> * mesh, int offset, int level, int start, int end, void * data) const;
-
-    virtual void ApplyBilinearEdgeVerticesKernel(FarMesh<OsdVertex> * mesh, int offset, int level, int start, int end, void * data) const;
-
-    virtual void ApplyBilinearVertexVerticesKernel(FarMesh<OsdVertex> * mesh, int offset, int level, int start, int end, void * data) const;
-
-
-    virtual void ApplyCatmarkFaceVerticesKernel(FarMesh<OsdVertex> * mesh, int offset, int level, int start, int end, void * data) const;
-
-    virtual void ApplyCatmarkEdgeVerticesKernel(FarMesh<OsdVertex> * mesh, int offset, int level, int start, int end, void * data) const;
-
-    virtual void ApplyCatmarkVertexVerticesKernelB(FarMesh<OsdVertex> * mesh, int offset, int level, int start, int end, void * data) const;
-
-    virtual void ApplyCatmarkVertexVerticesKernelA(FarMesh<OsdVertex> * mesh, int offset, bool pass, int level, int start, int end, void * data) const;
-
-
-    virtual void ApplyLoopEdgeVerticesKernel(FarMesh<OsdVertex> * mesh, int offset, int level, int start, int end, void * data) const;
-
-    virtual void ApplyLoopVertexVerticesKernelB(FarMesh<OsdVertex> * mesh, int offset, int level, int start, int end, void * data) const;
-
-    virtual void ApplyLoopVertexVerticesKernelA(FarMesh<OsdVertex> * mesh, int offset, bool pass, int level, int start, int end, void * data) const;
-
-    virtual void ApplyVertexEdits(FarMesh<OsdVertex> *mesh, int offset, int level, void * clientdata) const;
-
-    virtual void CopyTable(int tableIndex, size_t size, const void *ptr);
-
-    virtual void AllocateEditTables(int n);
-
-    virtual void UpdateEditTable(int tableIndex, const FarTable<unsigned int> &offsets, const FarTable<float> &values,
-                                 int operation, int primVarOffset, int primVarWidth);
-
-    virtual void OnKernelLaunch();
-
-    virtual void OnKernelFinish();
-
-    virtual OsdVertexBuffer *InitializeVertexBuffer(int numElements, int numVertices);
-
-    virtual void BindVertexBuffer(OsdVertexBuffer *vertex, OsdVertexBuffer *varying);
-
-    virtual void UnbindVertexBuffer();
-
-    virtual void Synchronize();
 
     virtual FarMesh<OsdVertex>::Strategy GetStrategy() {
         return FarMesh<OsdVertex>::SpMV;
     }
 
     static void Register();
+    virtual void BindVertexBuffer(OsdVertexBuffer *vertex, OsdVertexBuffer *varying);
 
     virtual void StageMatrix(int i, int j);
     virtual void StageElem(int i, int j, float value);
     virtual void PushMatrix();
     virtual void ApplyM(int offset);
 
-protected:
-
-    void WriteM();
-
-    // XXX: until far refactoring finishes, use this.
-    struct Table {
-        Table() : ptr(NULL) { }
-
-       ~Table();
-
-        void Copy(int size, const void *ptr);
-
-        void *ptr;
-    };
-
-    float *GetVertexBuffer() const { return _currentVertexBuffer ? _currentVertexBuffer->GetCpuBuffer() : NULL; }
-
-    float *GetVaryingBuffer() const { return _currentVaryingBuffer ? _currentVaryingBuffer->GetCpuBuffer() : NULL; }
+    virtual void WriteM();
 
     virtual int GetElemsPerVertex() const { return _currentVertexBuffer ? _currentVertexBuffer->GetNumElements() : NULL; }
-
     virtual int GetElemsPerVarying() const { return _currentVaryingBuffer ? _currentVaryingBuffer->GetNumElements() : NULL; }
-
-
-
-    OsdCpuVertexBuffer *_currentVertexBuffer,
-                       *_currentVaryingBuffer;
-
-    OskiVertexDescriptor *_vdesc;
-
-    std::vector<Table> _tables;
-    std::vector<Table> _editTables;
 
     oski_matrix_t A_tunable;
     oski_vecview_t x_view, y_view;
