76064ea552f78ddcb1b17913700a2eeab3af18fe
Michael Driscoll <mbdriscoll@gmail.com>
2012-11-21 14:48:04 -0800

    restart work on cusparse by subclassing MKL dispatcher

--- opensubdiv/osd/cusparseDispatcher.cpp
+++ opensubdiv/osd/cusparseDispatcher.cpp
@@ -14,13 +14,12 @@ namespace OpenSubdiv {
 namespace OPENSUBDIV_VERSION {
 
 OsdCusparseKernelDispatcher::OsdCusparseKernelDispatcher( int levels )
-    : OsdSpMVKernelDispatcher(levels), S(NULL), _currentVertexBuffer(NULL)
+    : OsdMklKernelDispatcher(levels)
 {
 }
 
 OsdCusparseKernelDispatcher::~OsdCusparseKernelDispatcher()
 {
-    if (S) delete S;
 }
 
 static OsdCusparseKernelDispatcher::OsdKernelDispatcher *
@@ -33,62 +32,6 @@ OsdCusparseKernelDispatcher::Register() {
     Factory::GetInstance().Register(Create, kCUSPARSE);
 }
 
-OsdVertexBuffer*
-OsdCusparseKernelDispatcher::InitializeVertexBuffer(int numElements, int numVertices) {
-    return new OsdCudaVertexBuffer(numElements, numVertices);
-}
-
-void
-OsdCusparseKernelDispatcher::BindVertexBuffer(OsdVertexBuffer *vertex, OsdVertexBuffer *varying) {
-
-    if (vertex) {
-        _currentVertexBuffer = dynamic_cast<OsdCudaVertexBuffer *>(vertex);
-        _numVertexElements = _currentVertexBuffer->GetNumElements();
-    } else {
-        _currentVertexBuffer = NULL;
-    }
-
-    if (_currentVertexBuffer)
-        _deviceVertices = (float*)_currentVertexBuffer->Map();
-}
-
-void
-OsdCusparseKernelDispatcher::UnbindVertexBuffer()
-{
-    if (_currentVertexBuffer){
-        _currentVertexBuffer->Unmap();
-    }
-}
-
-
-void
-OsdCusparseKernelDispatcher::StageMatrix(int i, int j)
-{
-    S = new coo_matrix(i,j);
-}
-
-inline void
-OsdCusparseKernelDispatcher::StageElem(int i, int j, float value)
-{
-#ifdef DEBUG
-    assert(0 <= i);
-    assert(i < S->size1());
-    assert(0 <= j);
-    assert(j < S->size2());
-#endif
-    (*S)(i,j) = value;
-}
-
-void
-OsdCusparseKernelDispatcher::PushMatrix()
-{
-    // csr_matrix A(*S);
-
-    // cusparseScsrgemm
-
-    delete S;
-}
-
 void
 OsdCusparseKernelDispatcher::FinalizeMatrix()
 {
@@ -97,41 +40,16 @@ OsdCusparseKernelDispatcher::FinalizeMatrix()
 void
 OsdCusparseKernelDispatcher::ApplyMatrix(int offset)
 {
-    float* V_in = _deviceVertices;
-    float* V_out = _deviceVertices + offset * _numVertexElements;
+    //float* V_in = _deviceVertices;
+    //float* V_out = _deviceVertices + offset * _numVertexElements;
 
     // cusparseScsrmv
-}
-
-void
-OsdCusparseKernelDispatcher::WriteMatrix()
-{
 }
 
 bool
 OsdCusparseKernelDispatcher::MatrixReady()
 {
     return true;
-}
-
-void
-OsdCusparseKernelDispatcher::PrintReport()
-{
-}
-
-// -------------------------------------------------------------------------------
-OsdCusparseKernelDispatcher::DeviceTable::~DeviceTable() {
-
-    if (devicePtr) cudaFree(devicePtr);
-}
-
-void
-OsdCusparseKernelDispatcher::DeviceTable::Copy(int size, const void *ptr) {
-
-    if (devicePtr)
-        cudaFree(devicePtr);
-    cudaMalloc(&devicePtr, size);
-    cudaMemcpy(devicePtr, ptr, size, cudaMemcpyHostToDevice);
 }
 
 } // end namespace OPENSUBDIV_VERSION
--- opensubdiv/osd/cusparseDispatcher.h
+++ opensubdiv/osd/cusparseDispatcher.h
@@ -2,62 +2,23 @@
 #define OSD_CUSPARSE_DISPATCHER_H
 
 #include "../version.h"
-#include "../osd/spmvDispatcher.h"
+#include "../osd/mklDispatcher.h"
 #include "../osd/cudaDispatcher.h"
-
-#include <boost/numeric/ublas/matrix_sparse.hpp>
-
-typedef boost::numeric::ublas::coordinate_matrix<
-    float,
-    boost::numeric::ublas::basic_row_major<int,int>,
-    0,
-    boost::numeric::ublas::unbounded_array<int>,
-    boost::numeric::ublas::unbounded_array<float>
-> coo_matrix;
 
 namespace OpenSubdiv {
 namespace OPENSUBDIV_VERSION {
 
-class OsdCusparseKernelDispatcher : public OsdSpMVKernelDispatcher
+class OsdCusparseKernelDispatcher : public OsdMklKernelDispatcher
 {
 public:
     OsdCusparseKernelDispatcher(int levels);
     virtual ~OsdCusparseKernelDispatcher();
 
     static void Register();
-    virtual OsdVertexBuffer *InitializeVertexBuffer(int numElements, int numVertices);
-    virtual void BindVertexBuffer(OsdVertexBuffer *vertex, OsdVertexBuffer *varying);
-    virtual void UnbindVertexBuffer();
 
-
-    virtual void StageMatrix(int i, int j);
-    virtual void StageElem(int i, int j, float value);
-    virtual void PushMatrix();
     virtual void ApplyMatrix(int offset);
-    virtual void WriteMatrix();
     virtual void FinalizeMatrix();
     virtual bool MatrixReady();
-    virtual void PrintReport();
-
-    coo_matrix *S;
-
-protected:
-    struct DeviceTable
-    {
-        DeviceTable() : devicePtr(NULL) {}
-       ~DeviceTable();
-
-        void Copy(int size, const void *ptr);
-
-        void *devicePtr;
-    };
-
-    std::vector<DeviceTable> _tables;
-    std::vector<DeviceTable> _editTables;
-
-    OsdCudaVertexBuffer *_currentVertexBuffer;
-    float *_deviceVertices;
-    int _numVertexElements;
 };
 
 } // end namespace OPENSUBDIV_VERSION
