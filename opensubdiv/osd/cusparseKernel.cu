2e191bccc4685be411c583125dd856e3466ab32c
Michael Driscoll <mbdriscoll@gmail.com>
2012-11-25 14:58:16 -0800

    get cuda threads vs blocks working

--- opensubdiv/osd/cusparseKernel.cu
+++ opensubdiv/osd/cusparseKernel.cu
@@ -1,13 +1,16 @@
 #include <stdio.h>
 
-#define THREADS_PER_BLOCK 4
+#define THREADS_PER_BLOCK 512
 
 __global__ void
-expand(int factor,
+expand(int src_numrows, int factor,
   int* dst_rows, int* dst_cols, float* dst_vals,
   int* src_rows, int* src_cols, float* src_vals)
 {
     int src_row = threadIdx.x + blockIdx.x * blockDim.x;
+    if (src_row >= src_numrows)
+        return;
+
     int v_per_row = src_rows[src_row+1] - src_rows[src_row];
     int base = src_rows[src_row];
 
@@ -28,7 +31,8 @@ OsdCusparseExpand(int src_numrows, int factor,
     int* dst_rows, int* dst_cols, float* dst_vals,
     int* src_rows, int* src_cols, float* src_vals)
 {
-    expand<<<src_numrows,1>>>(factor,
+    int blks = (src_numrows + THREADS_PER_BLOCK - 1) / THREADS_PER_BLOCK;
+    expand<<<blks,THREADS_PER_BLOCK>>>(src_numrows, factor,
             dst_rows, dst_cols, dst_vals,
             src_rows, src_cols, src_vals);
 }
