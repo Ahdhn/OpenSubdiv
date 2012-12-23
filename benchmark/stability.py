c0ffd966170b41e634609af8a0ecae53385f2d3b
Michael Driscoll <mbdriscoll@gmail.com>
2012-12-22 17:27:20 -0800

    got stability numbers from CPU vs MKL on my laptop

--- benchmark/stability.py
+++ benchmark/stability.py
@@ -8,7 +8,7 @@ modelLevel = {
     "BigGuy":       4,
     "Bunny":        4,
     "MonsterFrog":  4,
-    "Venus":        3,
+    "Venus":        4,
     "Cube":         7,
     "Icosahedron":  7,
 }
@@ -17,24 +17,24 @@ def build_db(model):
     db = set()
     for l in range( modelLevel[model] ):
         try:
-            run = do_run(frames=2, model=model, kernel="MKL", level=l)
+            run = do_run(frames=2, model=model, kernel="MKL", level=l+1)
             db.add(run)
         except ExecutionError as e:
             print "\tFailed with: %s" % e.message
     return db
 
-def gen_dat_file(ofile, db):
+def gen_dat_file(ofile, model, db):
     kernel_set = { r.kernel for r in db if r.kernel }
     size_set = { r.nverts for r in db if r.nverts }
     kernel_list = sorted(kernel_set, key=lambda k: kernelNum[k])
     size_list = sorted(size_set)
-    print >>ofile, "nVerts", " ".join(["%s %s-std" % (name, name) for name in kernel_list])
+    print >>ofile, "nVerts", escape_latex(model)
     for size in size_list:
         print >>ofile, size,
         for kernel in kernel_list:
             run_list = filter(lambda r: r.nverts == size and r.kernel == kernel, db)
             if len(run_list) == 1:
-                print >>ofile, " %f" % run_list[0].maxerror,
+                print >>ofile, " %e" % run_list[0].maxerror,
             if len(run_list) == 0:
                 print >>ofile, " ?",
         print >>ofile
@@ -42,7 +42,7 @@ def gen_dat_file(ofile, db):
 def main(argv):
     model = argv[1][10:-4]
     with open("stability_%s.dat" % model, 'w') as ofile:
-        gen_dat_file(ofile, build_db(model))
+        gen_dat_file(ofile, model, build_db(model))
 
 
 if __name__ == '__main__':
