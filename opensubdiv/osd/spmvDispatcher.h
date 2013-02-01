276e76d179d7ad06d2b6a52fe0a8edf2139f0629
Michael Driscoll <mbdriscoll@gmail.com>
2013-01-31 22:59:20 -0800

    fix mislabelling of nnz

--- opensubdiv/osd/spmvDispatcher.h
+++ opensubdiv/osd/spmvDispatcher.h
@@ -162,7 +162,7 @@ public:
         double sparsity_factor = 100.0 * SubdivOp->SparsityFactor();
 
         #if BENCHMARKING
-            printf(" nverts=%d", SubdivOp->nnz);
+            printf(" nnz=%d", SubdivOp->nnz);
             printf(" mem=%d", size_in_bytes);
             printf(" sparsity=%f", sparsity_factor);
         #endif
