5ff18397b76fe7cd271ad7f00afafff543fd12cd
Michael Driscoll <mbdriscoll@gmail.com>
2013-04-25 18:58:09 -0700

    use single loop for custom spmv. 40% faster

--- opensubdiv/osd/mklDispatcher.cpp
+++ opensubdiv/osd/mklDispatcher.cpp
@@ -85,31 +85,55 @@ void
 CpuCsrMatrix::logical_spmv(float* d_out, float* d_in) {
     omp_set_num_threads( omp_get_num_procs() );
 
-    #pragma omp parallel for
-    for(int i = 0; i < m; i++) {
+    int *__restrict__ rrows = &rows[0];
+    int *__restrict__ rcols = &cols[0];
+    float *__restrict__ rvals = &vals[0];
+
+    #pragma omp parallel
+    {
+
+        int size = omp_get_num_threads(),
+            rank = omp_get_thread_num(),
+            rows_per_thread = (m + size - 1) / size,
+            start_row = rank * rows_per_thread,
+            end_row = std::min(m, (rank+1) * rows_per_thread);
+
+        int row = start_row;
+        int start_k = rrows[start_row],
+            end_k = rrows[end_row];
 
         register __m128
             out03v = _mm_setzero_ps(),
             out45v = _mm_setzero_ps();
-        int out_idx = 6*i;
+        int out_idx, in_idx;
 
-        for (int k = rows[i]; k < rows[i+1]; k++) {
+        __m128 ignore, in03v, in45v, weightv;
 
-            int in_idx = 6*cols[k];
+        int next_row_k = rrows[row+1];
 
-            __m128 ignore,
-                   in03v = _mm_loadu_ps( &d_in[in_idx] ),
-                   in45v = _mm_loadl_pi( ignore, (const __m64*) &d_in[in_idx+4] ),
-                   weightv = _mm_load1_ps( &vals[k] );
+        for (int k = start_k; k < end_k; k++) {
 
-            out03v = _mm_add_ps(out03v, _mm_mul_ps(weightv, in03v));
-            out45v = _mm_add_ps(out45v, _mm_mul_ps(weightv, in45v));
+                in_idx = 6*rcols[k];
 
-            _mm_storeu_ps( &d_out[ out_idx ], out03v );
-            _mm_storel_pi( (__m64*) &d_out[ out_idx+4 ], out45v );
+                in03v = _mm_loadu_ps( &d_in[in_idx] );
+                in45v = _mm_loadl_pi( ignore, (const __m64*) &d_in[in_idx+4] );
+                weightv = _mm_load1_ps( &rvals[k] );
+
+                out03v = _mm_add_ps(out03v, _mm_mul_ps(weightv, in03v));
+                out45v = _mm_add_ps(out45v, _mm_mul_ps(weightv, in45v));
+
+                if (k+1 == next_row_k) {
+                    out_idx  = 6*row;
+                    _mm_storeu_ps( &d_out[ out_idx ], out03v );
+                    _mm_storel_pi( (__m64*) &d_out[ out_idx+4 ], out45v );
+                    out03v = _mm_setzero_ps();
+                    out45v = _mm_setzero_ps();
+                    row += 1;
+                    next_row_k = rrows[row+1];
+                }
+            }
         }
     }
-}
 
 void
 CpuCsrMatrix::spmv(float* d_out, float* d_in) {
