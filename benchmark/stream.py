97d37e36e548ce8720ea1c20252965319504b1d5
Michael Driscoll <mbdriscoll@gmail.com>
2013-04-30 15:18:52 -0700

    ran stream benchmark

--- benchmark/stream.py
+++ benchmark/stream.py
@@ -9,7 +9,7 @@ def build_db(model):
     for k in activeKernels:
         for l in range( modelMaxLevel[model] ):
             try:
-                run = do_run(frames=1000, model=model, kernel=k, level=l+1)
+                run = do_run(frames=10, model=model, kernel=k, level=l+1)
                 db.add(run)
             except ExecutionError as e:
                 print "\tFailed with: %s" % e.message
@@ -20,18 +20,18 @@ def gen_dat_file(ofile, db):
     size_set = { r.nverts for r in db if r.nverts }
     kernel_list = sorted(kernel_set, key=lambda k: kernelNum[k])
     size_list = sorted(size_set)
-    print >>ofile, "nVerts", " ".join(["%s %s-std" % (name, name) for name in kernel_list])
+    print >>ofile, "nVerts", " ".join(kernel_list)
     for size in size_list:
         print >>ofile, size,
         for kernel in kernel_list:
             run_list = filter(lambda r: r.nverts == size and r.kernel == kernel, db)
             if len(run_list) == 1:
                 run = run_list[0]
-                times = run.mean()
+                invtimes = run.mean()
                 if kernel in ['CPU', 'Cuda', 'OpenCL', 'OpenMP']:
-                    streambw = (run.tablemem + 6*4*run.nverts) / times
+                    streambw = (run.tablemem + 6*4*run.nverts) * invtimes / run.nverts / 1e6
                 else:
-                    streambw = (run.mem + 6*4*run.nverts) / times
+                    streambw = (run.mem + 6*4*run.nverts) * invtimes / run.nverts / 1e6
                 print >>ofile, " %f" % streambw,
             elif len(run_list) == 0:
                 print >>ofile, " ?",
